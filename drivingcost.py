4cba6852253cdd21b42c4084325e3bac6e7ef25f
kikiola <oluwatoby007@gmail.com>
2025-12-05 05:11:41 +0100

    Added new feature X

---
+++ drivingcost.py
@@ -0,0 +1,6 @@
+distance = float(input("Enter the driving distance"))
+miles_per_gallon = float(input("Enter miles per gallon"))
+price_per_gallon = float(input("Enter price per gallon"))
+
+total_cost = distance / miles_per_gallon * price_per_gallon
+print(total_cost)
