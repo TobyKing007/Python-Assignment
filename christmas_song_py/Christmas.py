3daa9e63a1e75bd65973be502777e8bce5e128d6
kikiola <oluwatoby007@gmail.com>
2025-12-18 17:02:55 +0100

    Add my initial Python assignment files

---
+++ christmas_song_py/Christmas.py
@@ -0,0 +1,124 @@
+for day in range(1, 13):
+
+
+    match day:
+        case 1:
+            day_name = "first"
+        case 2:
+            day_name = "second"
+        case 3:
+            day_name = "third"
+        case 4:
+            day_name = "fourth"
+        case 5:
+            day_name = "fifth"
+        case 6:
+            day_name = "sixth"
+        case 7:
+            day_name = "seventh"
+        case 8:
+            day_name = "eighth"
+        case 9:
+            day_name = "ninth"
+        case 10:
+            day_name = "tenth"
+        case 11:
+            day_name = "eleventh"
+        case 12:
+            day_name = "twelfth"
+
+    print(f"On the {day_name} day of Christmas my true love sent to me: ")
+
+ 
+    match day:
+        case 12:
+            print("Twelve drummers drumming,")
+            print("Eleven pipers piping,")
+            print("Ten lords a-leaping,")
+            print("Nine ladies dancing,")
+            print("Eight maids a-milking,")
+            print("Seven swans a-swimming,")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.\n")
+        case 11:
+            print("Eleven pipers piping,")
+            print("Ten lords a-leaping,")
+            print("Nine ladies dancing,")
+            print("Eight maids a-milking,")
+            print("Seven swans a-swimming,")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.\n")
+        case 10:
+            print("Ten lords a-leaping,")
+            print("Nine ladies dancing,")
+            print("Eight maids a-milking,")
+            print("Seven swans a-swimming,")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.\n")
+        case 9:
+            print("Nine ladies dancing,")
+            print("Eight maids a-milking,")
+            print("Seven swans a-swimming,")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.\n")
+        case 8:
+            print("Eight maids a-milking,")
+            print("Seven swans a-swimming,")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.\n")
+        case 7:
+            print("Seven swans a-swimming,")
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.\n")
+        case 6:
+            print("Six geese a-laying,")
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.\n")
+        case 5:
+            print("Five gold rings,")
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.\n")
+        case 4:
+            print("Four calling birds,")
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.\n")
+        case 3:
+            print("Three French hens,")
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.\n")
+        case 2:
+            print("Two turtle doves,")
+            print("And a partridge in a pear tree.\n")
+        case 1:
+            print("A partridge in a pear tree.\n")
+
